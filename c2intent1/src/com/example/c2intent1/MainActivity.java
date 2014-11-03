60e883d5fccd3a958fdad9050ece01498d288e68
cast7omadrid1 <Castro1162@gmail.com>
2014-11-03 18:42:41 +0100

    c2intent1

---
+++ c2intent1/src/com/example/c2intent1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.c2intent1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    public void onClick(View view) {
+        startActivity(new Intent(this, SecondActivity.class));
+        
+    }
+    
+
+}
