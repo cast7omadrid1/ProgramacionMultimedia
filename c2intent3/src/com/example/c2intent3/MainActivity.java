0b524bfe7e3c89575d19f64bf5e4af8bf5862b11
cast7omadrid1 <Castro1162@gmail.com>
2014-11-03 18:44:49 +0100

    c2intent3

---
+++ c2intent3/src/com/example/c2intent3/MainActivity.java
@@ -0,0 +1,43 @@
+/*
+Devolver resultados desde un intent
+Ejercicio pagina 86 libro android 4
+*/
+package com.example.c2intent3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    int request_Code = 1;
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    public void onClick(View view) {
+        //startActivity(new Intent("net.learn2develop.SecondActivity"));	
+        //or
+       //startActivity(new Intent("com.hueso.c21intent2.SecondActivity"));
+        
+       // Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ebookfrenzy.com"));
+	    //startActivity(intent);
+    	startActivityForResult(new Intent("com.example.c2intent3.SecondActivity"), request_Code);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == request_Code) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this,data.getData().toString(),
+                    Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+}
