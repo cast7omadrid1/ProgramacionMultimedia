f0f4549e8a3daf1b72e6eb04200073350600c9b7
cast7omadrid1 <Castro1162@gmail.com>
2014-11-27 19:18:58 +0100

    e315listaimagentexto1

---
+++ e315listaimagentexto1/gen/com/example/e315listaimagentexto1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.e315listaimagentexto1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ e315listaimagentexto1/src/com/example/e315listaimagentexto1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.e315listaimagentexto1;
+ 
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+ 
+public class MainActivity extends ListActivity {
+String[] itemname ={
+"Safari",
+"Camera",
+"Global",
+"FireFox",
+"UC Browser",
+"Android Folder",
+"VLC Player",
+"Cold War"
+};
+ 
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_main);
+this.setListAdapter(new ArrayAdapter<String>(
+this, R.layout.mylist,
+R.id.Itemname,itemname));
+}
+}
